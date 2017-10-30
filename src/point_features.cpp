8e59b1fc602e1929743829376fc581969d5e3884
andreucm <andreu@beta-robots.com>
2017-10-30 10:44:32 +0100

    Fixed waitKey duration and ogic to allow quit the program pressing q

--- src/point_features.cpp
+++ src/point_features.cpp
@@ -1,5 +1,5 @@
 
-//OpenCV 
+//OpenCV
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/features2d.hpp"
@@ -12,7 +12,7 @@
 //consts
 const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point fetaures
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     cv::VideoCapture camera; //OpenCV video capture object
     cv::Mat image; //OpenCV image object
@@ -20,28 +20,28 @@ int main(int argc, char *argv[])
     cv::Ptr<cv::ORB> orb_detector = cv::ORB::create(); //ORB point feature detector
     orb_detector->setMaxFeatures(MIN_NUM_FEATURES);
     std::vector<cv::KeyPoint> point_set; //set of point features
-    cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor 
-	
+    cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor
+
 	//check user args
 	switch(argc)
 	{
 		case 1: //no argument provided, so try /dev/video0
-			cam_id = 0;  
-			break; 
+			cam_id = 0;
+			break;
 		case 2: //an argument is provided. Get it and set cam_id
 			cam_id = atoi(argv[1]);
-			break; 
-		default: 
-			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl; 
-			std::cout << "EXIT program." << std::endl; 
-			break; 
+			break;
+		default:
+			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl;
+			std::cout << "EXIT program." << std::endl;
+			break;
 	}
-	
-	//advertising to the user 
+
+	//advertising to the user
 	std::cout << "Opening video device " << cam_id << std::endl;
 
     //open the video stream and make sure it's opened
-    if( !camera.open(cam_id) ) 
+    if( !camera.open(cam_id) )
 	{
         std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
         return -1;
@@ -51,29 +51,29 @@ int main(int argc, char *argv[])
     while(1)
 	{
 		//Read image and check it. Blocking call up to a new image arrives from camera.
-        if(!camera.read(image)) 
+        if(!camera.read(image))
 		{
             std::cout << "No image" << std::endl;
             cv::waitKey();
         }
-        		
+
     //**************** Find ORB point fetaures and descriptors ****************************
-        
+
         //clear previous points
-        point_set.clear(); 
-        
+        point_set.clear();
+
         //detect and compute(extract) features
         orb_detector->detectAndCompute(image, cv::noArray(), point_set, descriptor_set);
-        
+
         //draw points on the image
-        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );      
-                
+        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );
+
     //********************************************************************
-		
+
         //show image
         cv::imshow("Output Window", image);
 
-		//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
-        if(cv::waitKey(1) >= 0) break;
+		//Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
+    	if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
     }   
 }
